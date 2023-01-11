7f6d8edcad7ef43ce45cc78dcaa2693f25057bd7
fimovol <fimovolpe@gmail.com>
2023-01-11 13:18:20 -0500

    tarea asincrona

--- src/app/reto/reto.module.ts
+++ src/app/reto/reto.module.ts
@@ -2,18 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormularioRetoComponent } from './formulario-reto/formulario-reto.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { FormAsincronaComponent } from './form-asincrona/form-asincrona.component';
 
 
 @NgModule({
   declarations: [
-    FormularioRetoComponent
+    FormularioRetoComponent,
+    FormAsincronaComponent
   ],
   imports: [
     CommonModule,
     ReactiveFormsModule
   ],
   exports:[
-    FormularioRetoComponent
+    FormularioRetoComponent,
+    FormAsincronaComponent
   ]
 })
 export class RetoModule { }
